58b192cfec24b485b338deabdc6a5fb7e50aef9a
charb <30449853+aleccharb@users.noreply.github.com>
2017-10-18 21:03:19 -0400

    Final

---
+++ 99BottlesBeer/99BottlesBeer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BottlesBeer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int count = 99;
+            while (count>1)
+            {
+                Console.WriteLine(count + " bottles of beer on the wall " + count + " bottles of beer,");
+                count -= 1;
+                Console.WriteLine("take one down, pass it around " +count+ " bottles of beer on the wall." );
+                Console.WriteLine();
+            }
+            Console.WriteLine(count+ " bottle of beer on the wall " + count + " bottle of beer, ");
+            count -= 1;
+            Console.WriteLine("take one down, pass it around " + count + " bottles of beer on the wall.");
+        }
+    }
+}
